7bb1c7e2d6b2472cf6ce17e1632867b4b5e88702
Andrei Sili <andrei.sili1985@gmail.com>
2026-06-25 12:06:00 +0200

    feat(user): add Spring Data repositories

---
+++ services/user/src/main/java/com/ecommerce/user/repository/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.ecommerce.user.repository;
+
+import com.ecommerce.user.model.RefreshToken;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+  Optional<RefreshToken> findByTokenHash(String tokenHash);
+
+  @Modifying
+  @Query("UPDATE RefreshToken t SET t.revoked = true WHERE t.userId = :userId AND t.revoked = false")
+  int revokeAllForUser(@Param("userId") Long userId);
+}
